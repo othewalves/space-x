87a49429b45cdb4b5246c9ce6fec6f11f236ba75
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-03-14 12:25:09 -0300

    setup: config tailwindcss and add theme colors

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+    return (
+        <main>
+            <h1 className="text-primary">Home</h1>
+            <h1 className="text-primary-100">Home</h1>
+            <h1 className="text-primary-200">Home</h1>
+            <h1 className="text-primary-300">Home</h1>
+            <h1 className="text-primary-400">Home</h1>
+            <br />
+            <h1 className="text-secondary">Home</h1>
+            <h1 className="text-secondary-100">Home</h1>
+            <h1 className="text-secondary-200">Home</h1>
+            <h1 className="text-secondary-300">Home</h1>
+            <h1 className="text-secondary-400">Home</h1>
+            <br />
+            <h1 className="text-neutro-100">Home</h1>
+            <h1 className="text-neutro-500">Home</h1>
+            <h1 className="text-neutro-700">Home</h1>
+        </main >
+    );
+}
+
+export default Home;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<p>Home</p>} />
+                <Route path='/' element={<Home />} />
                 <Route path='/home' element={<p>Home 2</p>} />
                 <Route path='*' element={<Navigate to={'/'} />} />
             </Routes>
