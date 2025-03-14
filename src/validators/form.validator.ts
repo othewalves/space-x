b4c36a467e6c5b09f7004751497b9e855c936b0f
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-03-14 17:26:21 -0300

    refactor: add validation at description of disease

--- src/validators/form.validator.ts
+++ src/validators/form.validator.ts
@@ -8,7 +8,17 @@ export const formSchema = z.object({
         .refine((val) => !isNaN(val) && val >= 0, 'A idade não pode ser negativa')
         .refine((val) => val >= 18 && val <= 99, 'Você deve ser maior de 18 anos para embarcar nessa'),
     destiny: z.string().nonempty('Destino é obrigatório'),
-    hasDisease: z.enum(['yes', 'no'], { message: 'Opção obrigatória' })
+    hasDisease: z.enum(['yes', 'no'], { message: 'Opção obrigatória' }),
+    disease: z.string().optional()
 })
+    .refine((data) => {
+        if (data.hasDisease === 'yes' && !data.disease) {
+            return false;
+        }
+        return true
+    }, {
+        message: 'Descrição da doença é obrigatória',
+        path: ['disease']
+    })
 
 export type FormSchema = z.infer<typeof formSchema>;
