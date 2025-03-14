c1fa29d6215306ca416d07e3ea904f2d6df6a959
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-03-14 17:11:44 -0300

    feat: add input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,37 @@
 import { InputHTMLAttributes } from "react";
+import { Control, Controller } from 'react-hook-form';
+import { FormSchema } from "../../validators/form.validator";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
     label: string;
+    name: keyof FormSchema;
+    error: string;
+    control: Control<FormSchema>;
 }
 
-const Input = ({ label, ...rest }: IInputProps) => {
+const Input = ({
+    label,
+    name,
+    control,
+    error,
+    ...rest
+
+}: IInputProps) => {
     return (
-        <div>
+        <div className='w-full'>
             <label className="text-sm font-medium text-left text-neutro-500">{label}</label>
-            <input
-                className="w-full h-14 px-4 rounded-xl border border-neutro-500 outline-none"
-                {...rest}
+            <Controller
+                name={name}
+                control={control}
+                render={({ field }) => (
+                    <input
+                        {...field}
+                        className="w-full h-14 px-4 rounded-xl border border-neutro-500 outline-none"
+                        {...rest}
+                    />
+                )}
             />
+            {error && <p className="text-red-500 text-sm w-full">{error}</p>}
         </div>
     );
 }
